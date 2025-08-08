b31f05e4bf3e6c1ac652f6a70ce164d8ef239c07
sahitidoke <122229647+sahitidoke@users.noreply.github.com>
2025-08-08 02:50:11 -0700

    Update recommendation_engine.py

--- recommendation_engine.py
+++ recommendation_engine.py
@@ -62,7 +62,7 @@ class Resume:
             token.lemma_.lower() for token in doc
             if token.is_alpha and not token.is_stop and token.pos_ in ['NOUN', 'PROPN']
         ])
-    def generate_summary():
+    def generate_summary(self):
         inputs = tokenizer.encode("summarize: " + self.filtered_text, return_tensors="pt", max_length=1024, truncation=True)
         summary_ids = model.generate(inputs, max_length=150, min_length=50, length_penalty=2.0, num_beams=4, early_stopping=True)
         summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
