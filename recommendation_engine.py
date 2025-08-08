950b1342c844ad4214492f06c0651c71b3033597
sahitidoke <122229647+sahitidoke@users.noreply.github.com>
2025-08-08 03:08:42 -0700

    Update recommendation_engine.py

--- recommendation_engine.py
+++ recommendation_engine.py
@@ -6,7 +6,7 @@ import re
 import torch
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
-from transformers import pipeline
+from transformers import BartTokenizer, BartForConditionalGeneration
 
 
 
@@ -64,8 +64,12 @@ class Resume:
             if token.is_alpha and not token.is_stop and token.pos_ in ['NOUN', 'PROPN']
         ])
     def generate_summary(self):
-        summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-        self.summary = summarizer(self.filtered_text, max_length=130, min_length=30)
+        tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
+        model = BartForConditionalGeneration.from_pretrained("facebook/bart-large-cnn")
+    
+        inputs = tokenizer(self.filtered_text, max_length=1024, return_tensors="pt", truncation=True)
+        summary_ids = model.generate(inputs["input_ids"], max_length=130, min_length=30, length_penalty=2.0, num_beams=4, early_stopping=True)
+        self.summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
         return self.summary
 
 class ResumeMatcher:
