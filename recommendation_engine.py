2c9a222e70d553b98697267478092b426fe1d3a6
sahitidoke <122229647+sahitidoke@users.noreply.github.com>
2025-08-08 02:55:37 -0700

    Update recommendation_engine.py

--- recommendation_engine.py
+++ recommendation_engine.py
@@ -65,8 +65,8 @@ class Resume:
     def generate_summary(self):
         inputs = tokenizer.encode("summarize: " + self.filtered_text, return_tensors="pt", max_length=1024, truncation=True)
         summary_ids = model.generate(inputs, max_length=150, min_length=50, length_penalty=2.0, num_beams=4, early_stopping=True)
-        summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-        return summary
+        self.summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+        return self.summary
 
 class ResumeMatcher:
     def __init__(self, job_description):
